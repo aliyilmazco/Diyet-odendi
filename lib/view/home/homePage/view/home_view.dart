14ef8d69f5e395233d1164dd8e64aea7f0d15e8a
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-04-05 04:11:28 +0300

    feature: main container first lookup

--- lib/view/home/homePage/view/home_view.dart
+++ lib/view/home/homePage/view/home_view.dart
@@ -2,6 +2,7 @@ import 'package:d/core/base/view/base_view.dart';
 import 'package:d/core/constant/color_constant.dart';
 import 'package:d/view/home/homePage/viewmodel/home_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomeView extends StatefulWidget {
@@ -60,6 +61,37 @@ class _HomeViewState extends HomeViewModel {
                     ),
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Card(
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                elevation: 5,
+                clipBehavior: Clip.hardEdge,
+                child: Container(
+                  color: ColorConst.mainBoxBg,
+                  width: 300,
+                  height: 300,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/breakfast2.png',
+                            width: 200,
+                          ),
+                          Text('asdasdasd'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
