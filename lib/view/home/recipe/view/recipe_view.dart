d4701c7fe9d3fe4eb86c42a9338733025f3ec57b
alyilmaz99 <aliyilmaz9977@gmail.com>
2023-04-09 18:24:48 +0300

    feature: recipe page update

--- lib/view/home/recipe/view/recipe_view.dart
+++ lib/view/home/recipe/view/recipe_view.dart
@@ -1,8 +1,10 @@
 import 'package:d/core/base/view/base_view.dart';
 import 'package:d/core/constant/color_constant.dart';
+import 'package:d/product/widget/create/selection_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class RecipeView extends StatefulWidget {
@@ -23,20 +25,79 @@ class _RecipeViewState extends State<RecipeView> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Padding(
-                padding: EdgeInsets.only(top: height / 40, right: width / 2.2),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Hosgeldin',
-                      style: GoogleFonts.glory(
-                        color: ColorConst.createPageText,
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold,
+                padding: EdgeInsets.only(top: height / 20, right: width / 3.4),
+                child: Text(
+                  'Saglikli Tariflerim',
+                  style: GoogleFonts.glory(
+                    color: ColorConst.createPageText,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                height: 70,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: ColorConst.appBgColorWhite,
+                      width: 150,
+                      margin: const EdgeInsets.all(8),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: ColorConst.mainBoxBg,
+                          elevation: 4,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg/ana_ogun.svg',
+                              width: 30,
+                              height: 30,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Ana Ogun',
+                              style: GoogleFonts.glory(
+                                color: ColorConst.createPageText,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Row(),
-                  ],
+                    );
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                height: height / 2,
+                color: ColorConst.mainBoxBg,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: ((context, index) {
+                    return Stack(
+                      children: [
+                        Image.asset('assets/images/salad.png',
+                            width: width / 1.3),
+                      ],
+                    );
+                  }),
                 ),
               ),
             ],
